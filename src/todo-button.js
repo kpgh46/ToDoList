5b2303e060ebb9ceb5aabc9f4ce1f0f1a7f28a88
Kevin McPeak <kevmc46@gmail.com>
2021-12-12 12:07:41 -0500

    added error message when no values entered

--- src/todo-button.js
+++ src/todo-button.js
@@ -14,6 +14,7 @@ let addToDo = (() => {
         let todoDivHeader = document.createElement("h3");
         todoDivHeader.textContent = "Add To Do:"
         todoDivHeader.classList.add("small-div-header");
+        todoDivHeader.id = "todo-div-header";
         createNewToDoDiv.appendChild(todoDivHeader);
 
         let todoFields = ["title", "description", "date", "priority"]
@@ -38,46 +39,67 @@ let addToDo = (() => {
         document.querySelector("#mainpage").appendChild(createNewToDoDiv);
     }
 
+    //need to create timeout feature
+    let noValueError = (parent) => {
+
+        let errorMessage = document.createElement('div');
+        errorMessage.classList.add("error-message");
+        errorMessage.id = "todo-error";
+        errorMessage.textContent = "Missing value";
+
+        parent.parentNode.insertBefore(errorMessage,parent);
+
+        // clearTimeout(errorMessage.remove(), 2000)
+
+    }
+
     let submitToDo = (event) => {
 
         let el = event.target;
-
-
+        let header = document.querySelector('#todo-div-header');
 
         if (el.id === "create-todo"){
 
             let title1 = document.querySelector("#title").value;
+            let test = document.querySelector("#title");
             let description2 = document.querySelector("#description").value;
             let date3 = document.querySelector("#date").value;
             let priority4 = document.querySelector("#priority").value;
 
-            let newToDo = (title,description,date, priority) => {
-                return {title, description, date, priority}
-            }
-            
-            let toDoDivList = document.createElement('div');
-            toDoDivList.classList.add("project-entry");
+            if (title1 === '' || description2 === '' || date3 === '' || priority4 === ''){
+                noValueError(test);
+            }else{
 
-            let toDoObject = newToDo(title1,description2,date3,priority4);
-            let toDoObjectArr = Object.values(toDoObject);
-
-            console.log(toDoObject);
-            console.log(Object.values(toDoObject));
-           
-
-            Object.entries(toDoObject).forEach(el => {
-
-                let miniDiv = document.createElement('div');
-                miniDiv.classList.add('mini-div');
-                miniDiv.textContent = el;
-
-                toDoDivList.appendChild(miniDiv);
+                let newToDo = (title,description,date, priority) => {
+                    return {title, description, date, priority}
+                }
                 
-            });
+                let toDoDivList = document.createElement('div');
+                toDoDivList.classList.add("project-entry");
 
-            document.querySelector("#new-todo").appendChild(toDoDivList);
+                let toDoObject = newToDo(title1,description2,date3,priority4);
+                let toDoObjectArr = Object.values(toDoObject);
 
-            document.querySelector("#new-todo-div").value = '';
+                console.log(toDoObject);
+                console.log(Object.values(toDoObject));
+            
+
+                Object.entries(toDoObject).forEach(el => {
+
+                    let miniDiv = document.createElement('div');
+                    miniDiv.classList.add('mini-div');
+                    miniDiv.textContent = el;
+
+                    toDoDivList.appendChild(miniDiv);
+                    
+                });
+
+                document.querySelector("#new-todo").appendChild(toDoDivList);
+
+                document.querySelector("#new-todo-div").remove();
+            };
+            
+            
         }
 
     }
